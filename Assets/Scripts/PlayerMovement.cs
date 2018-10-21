c7a02643aa00dea21b3e8dd720ac56fd06c55aa4
Nekith <nekith@gmail.com>
2018-10-21 20:00:45 +0200

    Remove hovering

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,18 +6,13 @@ public class PlayerMovement : MonoBehaviour
 {
 	public float sensitivity = 1.0f;
 	public float maxSpeed = 10.0f;
-	public float maxHoverSpeed = 17.0f;
 	public float acceleration = 4.0f;
 	public float deceleration = 1.0f;
-	public float hoverFov = 80.0f;
-	float basicFov;
 	Vector3 velocity;
 	float verticalRotation;
-	bool hovering;
 	Rigidbody rigid;
 	CapsuleCollider capsule;
 	Transform eyes;
-	Camera eyesCamera;
 
 	void Start()
 	{
@@ -25,8 +20,6 @@ public class PlayerMovement : MonoBehaviour
 		Cursor.visible = false;
 		rigid = GetComponent<Rigidbody>();
 		eyes = transform.Find("MainCamera");
-		eyesCamera = eyes.GetComponent<Camera>();
-		basicFov = eyesCamera.fieldOfView;
 	}
 
 	void Update()
@@ -37,18 +30,12 @@ public class PlayerMovement : MonoBehaviour
 		eyes.localEulerAngles = Vector3.left * verticalRotation;
 		float h = Input.GetAxisRaw("Horizontal") * sensitivity;
 		float v = Input.GetAxisRaw("Vertical") * sensitivity;
-		hovering = Input.GetButton("Hovering");
 		if (h != 0 || v != 0) {
 			velocity += (transform.right * h + transform.forward * v).normalized * acceleration;
 		} else {
 			velocity -= velocity.normalized * deceleration;
 		}
-		velocity = Vector3.ClampMagnitude(velocity, hovering ? maxHoverSpeed : maxSpeed);
-		if (hovering) {
-			eyesCamera.fieldOfView = Mathf.Lerp(eyesCamera.fieldOfView, hoverFov, Time.deltaTime * 10);
-		} else {
-			eyesCamera.fieldOfView = Mathf.Lerp(eyesCamera.fieldOfView, basicFov, Time.deltaTime * 10);
-		}
+		velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
 	}
 
 	void FixedUpdate()
